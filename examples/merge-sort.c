11ea52b043d41cd27bb043991b4ddf663d475981
chengWei1123 <c09721123@gmail.com>
2019-02-26 22:03:31 +0800

    Construct the program structure of merge-sort.c
    by mirroring quick-sort.c
    
    Add 2 functions:
      1. list_merge: merge 2 sorted list l1, l2 into another
    		sorted list pointed by head
      2. list_msort: merge sort on a list
    But no yet implement the actual functionality

---
+++ examples/merge-sort.c
@@ -0,0 +1,56 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/list.h"
+
+#include "../private/common.h"
+
+static uint16_t values[256];
+
+static void list_merge(struct list_head *head,
+                       struct list_head *l1,
+                       struct list_head *l2)
+{
+    return;
+}
+static void list_msort(struct list_head *head)
+{
+    return;
+}
+int main(void)
+{
+    struct list_head testlist;
+    struct listitem *item, *is = NULL;
+    size_t i;
+
+    random_shuffle_array(values, (uint16_t) ARRAY_SIZE(values));
+
+    INIT_LIST_HEAD(&testlist);
+
+    assert(list_empty(&testlist));
+
+    for (i = 0; i < ARRAY_SIZE(values); i++) {
+        item = (struct listitem *) malloc(sizeof(*item));
+        assert(item);
+        item->i = values[i];
+        list_add_tail(&item->list, &testlist);
+    }
+
+    assert(!list_empty(&testlist));
+
+    qsort(values, ARRAY_SIZE(values), sizeof(values[0]), cmpint);
+    list_msort(&testlist);
+
+    i = 0;
+    list_for_each_entry_safe (item, is, &testlist, list) {
+        assert(item->i == values[i]);
+        list_del(&item->list);
+        free(item);
+        i++;
+    }
+
+    assert(i == ARRAY_SIZE(values));
+    assert(list_empty(&testlist));
+
+    return 0;
+}
